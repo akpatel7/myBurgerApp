941a562ca9fc2e7667f5570630bcb98bc8f926b7
Amit Patel <patel_amitk@hotmail.com>
2019-09-07 18:43:42 -0400

    Burger App v0.1 finished, improved modal rerender performance, Next:  Server, Redux, Hooks

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,11 +9,7 @@ class Modal extends Component {
     shouldComponentUpdate(nextProps, nextState) {
         return nextProps.show !== this.props.show
     }
-
-    componentWillUpdate() {
-        console.log('[Modal] WillUpdate');
-    }
-
+    
     render() {
         return (
             <Aux>
