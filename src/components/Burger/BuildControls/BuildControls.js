3428ba30b022a793f06fd10bd3cb78137d2cc8a1
Amit Patel <patel_amitk@hotmail.com>
2019-09-06 21:31:37 -0400

    Intial Commit

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import classes from './BuildControls.css';
+import BuildControl from './BuildControl/BuildControl';
+
+const controls = [
+    { label: 'salad', type: 'salad'},
+    { label: 'bacon', type: 'bacon'},
+    { label: 'cheese', type: 'cheese'},
+    { label: 'meat', type: 'meat'}
+];
+
+const buildControls = (props) => (
+    <div className={classes.BuildControls}>
+        {controls.map(ctrl => (
+            <BuildControl key={ctrl.label} label={ctrl.label}/>
+        ))}
+    </div>
+);
+
+export default buildControls;
