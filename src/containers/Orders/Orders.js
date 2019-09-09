9f8253fc7af2b9ee28aed72e4eae16ba0656355e
Amit Patel <patel_amitk@hotmail.com>
2019-09-08 22:19:29 -0400

    Added routing to Orders page, and send order, fetch orders from firebase, Next: Checkout Form

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react';
 
+import axios from '../../axios-orders';
+
 import classes from './Orders.css';
 import Order from '../../components/Order/Order/Order';
+import withErrorHandler from '../../hoc/WithErrorHandler/WithErrorHandler';
 
 class Orders extends Component {
+    state = {
+        orders: [],
+        loading: true
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then(res => {
+                const fetchedOrders = [];
+                for (let key in res.data) {
+                    fetchedOrders.push({
+                        id: key,
+                        ...res.data[key]
+                    });
+                }
+                this.setState({orders: fetchedOrders, loading: false});
+            })
+            .catch(err => {
+                this.setState({orders: [], loading: false});
+            });
+    }
+
     render() {
         return (
             <div className={classes.Orders}>
-                <Order />
-                <Order />
+                {this.state.orders.map(order => {
+                    return (
+                        <Order 
+                            key={order.id}
+                            ingredients={order.ingredients}
+                            price={order.price} />
+                    );
+                })}
             </div>
         );
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
